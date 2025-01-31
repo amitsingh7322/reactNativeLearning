223da114d45bae4eb67be4f89c8ddf13a288ef9f
Amit Singh <royalamit7322@gmail.com>
2025-02-01 00:08:01 +0530

    initialize nativewind

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-500">
+      <Text className="text-white text-lg">Hello, NativeWind!</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./**/*.html",
+    "./**/*.js",
+    "./**/*.ts",
+    "./**/*.jsx",
+    "./**/*.tsx",
+    "./App.js",
+    "./node_modules/nativewind/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
